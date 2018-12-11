a55ff90d340fcca70da41f060f8941220513efdb
Brian G <brian.gregorius@gmail.com>
2018-12-10 21:34:48 -0500

    styled intro page

--- storytime/src/components/Intro.js
+++ storytime/src/components/Intro.js
@@ -1,15 +1,18 @@
 import React from 'react';
-
+import './intro.css';
 
 const Intro = props => {
     console.log('intro', props);
 
     return (
-        <div className='intro'>
-            <img src='https://readstorytime.com/images/logo@2x.png' className="storyClearLogo" alt="logo" />
-            <h1>{props.data.tagLine}</h1>
-            <img src='https://readstorytime.com/images/intro_screen.png' className="storyTimeIphone" alt="logo" />
-
+        <div className='intro-container'>
+            <div className='intro'>
+                <div className='intro-logo-tag'>
+                    <img src='https://readstorytime.com/images/logo@2x.png' className="storyClearLogo" alt="logo" />
+                    <h1>{props.data.tagLine}</h1>
+                </div>
+                <img src='https://readstorytime.com/images/intro_screen.png' className="storyTimeIphone" alt="logo" />
+            </div>
         </div>
     )
 };
