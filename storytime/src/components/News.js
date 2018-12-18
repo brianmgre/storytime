91d112dabb2ae3abdce25f9e64cc9f96cc371a84
Brian G <brian.gregorius@gmail.com>
2018-12-17 21:42:26 -0500

    added styling for news page

--- storytime/src/components/News.js
+++ storytime/src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { updatesNews } from '../websiteData';
+import '../css/news.css';
 
 
 class News extends Component {
@@ -17,14 +18,16 @@ class News extends Component {
 
     render() {
         return (
-            <div>
-                <h1>NEWS &amp; Updates</h1>
+            <div className= 'news-wrapper'>
+                <h1>News &amp; Updates</h1>
                 {this.state.newsData.map(news => (
                     <div key={news.title}>
+                    <div className='indiv-post'>
                         <h2>{news.title}</h2>
                         <p>{news.date}</p>
                         <img src={news.imgUrl} className="storyTimeIphone" alt="logo" />
                         <p>{news.text}</p>
+                        </div>
                     </div>
                 ))}
             </div>
